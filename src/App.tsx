5b62c460a6e16f3e6658ee68e6efe06a44bb27ca
YKavaliou <yulian.kov@gmail.com>
2023-01-25 15:49:44 +0100

    feat: migrate to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,42 @@
-import React, { Component } from "react";
-import _ from "lodash";
+import React, { useEffect, useState } from 'react';
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 import { IVideo } from './types';
+import _ from 'lodash';
 
 export const API_KEY = "AIzaSyCbcQMTPqAevOao2BQsQadm5SFTZljP2dM";
 
-interface IProps {}
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState<IVideo>();
+  const [videos, setVideos] = useState<IVideo[]>([]);
 
-interface IState {
-  videos: IVideo[];
-  selectedVideo: IVideo | null;
-}
-
-class App extends Component<IProps, IState> {
-  /**
-   * Lifecycle method that initializes the state of the component
-   * @param {*} props
-   */
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-    };
-  }
-
-  /**
-   * Lifecycle method that is being called just after the component did mount
-   */
-  componentDidMount() {
-    this.videoSearch("liverpool");
-  }
-
-  /**
-   * Function that gets the search-term and returns a list of videos
-   * @param {*} term
-   */
-  videoSearch(term: string) {
-    YTSearch({ key: API_KEY, term: term }, (videos: IVideo[]) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0],
-      });
+  const videoSearch = (searchPhrase: string) => {
+    YTSearch({ key: API_KEY, term: searchPhrase }, (dataVideos: IVideo[]) => {
+      setVideos(dataVideos);
+      setSelectedVideo(dataVideos[0])
     });
   }
-
-  /**
-   * Lifecycle method that is responsible to make the component visible to the browser
-   */
-  render() {
-    const videoSearch = _.debounce((term) => {
-      this.videoSearch(term);
-    }, 300);
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={(selectedVideo: IVideo) => this.setState({ selectedVideo })}
-          videos={this.state.videos}
-        />
-      </div>
-    );
-  }
+  
+  const debouceSearch = _.debounce((term) => {
+    videoSearch(term);
+  }, 300);
+  
+  useEffect(() => {
+    videoSearch("liverpool")
+  }, []);
+  
+  return (
+    <div>
+      <SearchBar onSearchTermChange={debouceSearch} />
+      <VideoDetail video={selectedVideo} />
+      <VideoList
+        onVideoSelect={(selectedVideo: IVideo) => setSelectedVideo(selectedVideo)}
+        videos={videos}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/search_bar.tsx
+++ src/components/search_bar.tsx
@@ -1,38 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 
 interface IProps {
   onSearchTermChange: (term: string) => void
 }
 
-interface IState {
-  term: string;
-}
-
-class SearchBar extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = { term: "" };
+const SearchBar: React.FC<IProps> = ({onSearchTermChange}) => {
+  const [term, setTerm] = useState("");
+  
+  const onInputChange = (searchString: string) => {
+    setTerm(searchString);
+    onSearchTermChange(searchString);
   }
 
-  render() {
-    return (
-      <div className="search-bar">
-        <input
-          value={this.state.term}
-          onChange={(event) => this.onInputChange(event.target.value)}
-        />
-      </div>
-    );
-  }
-
-  /**
-   * Function that is being called every time the input has been changed
-   * @param {*} term
-   */
-  onInputChange(term: string) {
-    this.setState({ term });
-    this.props.onSearchTermChange(term);
-  }
+  return (
+    <div className="search-bar">
+      <input
+        value={term}
+        onChange={(event) => onInputChange(event.target.value)}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
